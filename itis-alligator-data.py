ae5af6928e41463b929faeef4e26431e3784805c
Deya Liao <deya.liao@gmail.com>
2022-05-25 14:28:48 -0700

    Updated query more accuracy

--- itis-alligator-data.py
+++ itis-alligator-data.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import json
 import pandas as pd
+import numpy as np
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -18,11 +19,13 @@ def get_results(endpoint_url, query):
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
+# taxon name: P225
+# taxon common name: P1843
 # Get alligator id, definition, prelabel (use with triple style code)
 def animal_id(animal):
     query = "SELECT ?item ?itemLabel ?itemDescription  \
         WHERE \
-            {?item rdfs:label \"" + animal + "\"@en. \
+            {?item wdt:P225 \"" + animal + "\" . \
             SERVICE wikibase:label \
             {bd:serviceParam wikibase:language \"en\" . } \
         }"
@@ -41,7 +44,7 @@ def animal_id(animal):
             id = url[n:]
             n -= 1
         print(id + "  " + label + "  " + description)
-        return id, label, description
+        return id, label.capitalize(), description.capitalize()
     return id, "", ""
     
 
@@ -68,7 +71,6 @@ def parent_ID(parent_taxon):
         return parent_taxon
 
 # Get taxon rank property (faulty)
-
 def taxon_rank(rank):
     query = "SELECT ?x ?p ?wdt WHERE { \
         ?x rdfs:label \"" + rank + "\"@en;  \
@@ -112,46 +114,63 @@ def taxonomic_hierarchy():
             
 df = pd.read_csv("itis/taxa_9796598.txt", sep='\t')
 
-# clean up dataframe 
-df = df.loc[df["taxonomicStatus"] == "valid"]
-updated_names = [a.replace(" " + b, "") for (a, b) in zip(df["scientificName"].values, df["scientificNameAuthorship"].fillna('').values)]
-df["scientificName"] = updated_names
-print(df)
-
-
 # extraneous information
 type = df.dtypes
+print(type)
 num_rows = len(df.count(axis=1))
 num_columns = len(df.count(axis=0))
 
-# convert to readable csv file
-df.to_csv('itis.csv')
-
 # open ttl file
-outfile = open('itis-alligator-data.ttl', 'a')
+outfile = open('itis-alligator-data.ttl', 'w')
+
+outfile.write("@prefix boltz: <http://solid.boltz.cs.cmu.edu:3030/data/> . \n\
+@prefix kgo:   <http://solid.boltz.cs.cmu.edu:3030/ontology/> . \n\
+@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . \n\
+@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> . \n\
+@prefix owl:   <http://www.w3.org/2002/07/owl#> . \n\
+@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n\
+@prefix qudt:  <http://qudt.org/schema/qudt/> . \n\
+@prefix unit:  <http://qudt.org/vocab/unit/> . \n\
+@prefix qkdv:  <http://qudt.org/vocab/dimensionvector/> . \n\
+@prefix sou: <http://qudt.org/vocab/sou/> . \n\
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> . \n\n\
+")
 
 #keep track of prev ID, used for subParentID
-ID = "" 
-full_parent_ID = ""
+q_values = {}
 
 # convert data to triples here 
 for index, row in df.iterrows():
-    if index != 0: 
-        parent_id = ID
+    # check row: already looked up OR not valid
+    if row["taxonID"] in q_values or not row['taxonomicStatus'] == "valid":
+        continue
+
+    scientificName = row["scientificName"]
+    # change scientificName, some have author appended onto it
+    if not pd.isna(row["scientificNameAuthorship"]) and row["scientificNameAuthorship"]in row["scientificName"]:
+        scientificName = row["scientificName"].replace(" " + row["scientificNameAuthorship"], "")
+
+    # subTaxon dictionary lookup
+    if not pd.isna(row["parentNameUsageID"]) and int(row["parentNameUsageID"]) in q_values: 
+        parent_id = q_values[int(row["parentNameUsageID"])]
         full_parent_ID = "kgo:subTaxonOf\tboltz:" + parent_id + " ; \n\t"
+    else:
+        full_parent_ID = ""
     
-    print(row['scientificName'])
-    ID, label, definition = animal_id(row['scientificName'])
-    full_name = "kgo:taxonName \t" + "\"" + row['scientificName'] + "\"@en ; \n\t"
+    ID, label, definition = animal_id(scientificName)
+    full_name = "kgo:taxonName \t" + "\"" + scientificName + "\"@en ; \n\t"
     full_ID = "boltz:" + ID + " a kgo:Taxon ; \n\t" #ID
     full_rank = "kgo:taxonRank\tkgo:" + row['taxonRank'].capitalize() + " ; \n\t" #rank
     full_definition = "skos:definition\t\"" + definition + "\"@en ;\n\t"
+
+    # preflabel – common name
     full_label = "skos:prefLabel\t\"" + label + "\"@en .\n\n"
     
+    # update dict
+    q_values[row["taxonID"]] = ID
         
     d =  full_ID + full_parent_ID + full_name + full_rank + full_definition + full_label
     outfile.write(d)
 
-
-
 outfile.close()
+print(q_values)
